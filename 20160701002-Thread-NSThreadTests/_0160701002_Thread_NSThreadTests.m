80af502d271c4ddb33b5e3696ee59dc5e2fd0859
Rainer <it_jinlei.yue@163.com>
2016-07-01 15:57:27 +0800

    Initial Commit

---
+++ 20160701002-Thread-NSThreadTests/_0160701002_Thread_NSThreadTests.m
@@ -0,0 +1,39 @@
+//
+//  _0160701002_Thread_NSThreadTests.m
+//  20160701002-Thread-NSThreadTests
+//
+//  Created by Rainer on 16/7/1.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0160701002_Thread_NSThreadTests : XCTestCase
+
+@end
+
+@implementation _0160701002_Thread_NSThreadTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
